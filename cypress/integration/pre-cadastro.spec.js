737b8bc0820e00eef2eb20990ccf9d9cedb56776
Letícia Cardoso <leticiacardoso31@hotmail.com>
2023-01-02 20:27:06 -0300

    commit aula 12.6

--- cypress/integration/pre-cadastro.spec.js
+++ cypress/integration/pre-cadastro.spec.js
@@ -24,4 +24,9 @@ describe('Funcionalidade Pré Cadastro', () => {
         cy.get('.woocommerce-message').should('contain', 'Detalhes da conta modificados com sucesso.')
     });
 
+    it.only('Deve completar o pré-cadastro com sucesso usando comandos customizados', () => {
+        let emailFaker2 = faker.internet.email()
+        cy.preCadastro(emailFaker2, 'senha!@#forte', 'Letícia', 'Cardoso')
+        cy.get('.woocommerce-message').should('contain', 'Detalhes da conta modificados com sucesso.')
+    });
 });
--- cypress/integration/produtos.spec.js
+++ cypress/integration/produtos.spec.js
@@ -16,16 +16,21 @@ describe('Funcionalidade Página de produtos', () => {
 
     });
 
-    it.only('Deve adicionar um produto ao carrinho', () => {
+    it('Deve adicionar um produto ao carrinho', () => {
+        var quantidade = 2
         cy.get('[class="product-block grid"]')
             .contains('Arcadio Gym Short').click()
         cy.get('.button-variable-item-34').click()
         cy.get('.button-variable-item-Blue').click()
-        cy.get('.input-text').clear().type(2)
+        cy.get('.input-text').clear().type(quantidade)
         cy.get('.single_add_to_cart_button').click()
 
         cy.get('.dropdown-toggle > .mini-cart-items').should('contain', 2)
-        cy.get('.woocommerce-message').should('contain', 'foram adicionados no seu carrinho')
+        cy.get('.woocommerce-message').should('contain', quantidade + 'foram adicionados no seu carrinho')
+    });
+
+    it.only('Deve adicionar produtos ao carrinho - Usando Comando customizados', () => {
+        cy.addProdutos('Arcadio Gym Short', 2)
     });
 
 });
